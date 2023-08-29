b443439e5ad8539c798eed47cde74531bffa3474
Mohamed Adel <mohamed101291@gmail.com>
2023-08-29 16:46:09 +0300

    Mag App Test React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Home from "./HomePage/home.jsx";
-import About from "./AboutPage/about.jsx";
+import Home from "./components/HomePage/home.jsx";
+import About from "./components/AboutPage/about.jsx";
+import Products from "./components/ProductsPage/products.jsx";
 import {
     BrowserRouter,
     Routes,
     Route,
-    Router,
+    // Router,
     Link,
     NavLink,
 } from "react-router-dom";
@@ -16,10 +17,12 @@ const App = () => {
             <NavLink>
                 <Link to="/Home">Home</Link>
                 <Link to="/About">About</Link>
+                <Link to="/Products">Products</Link>
             </NavLink>
             <Routes>
                 <Route path="/Home" element={<Home />} />
                 <Route path="/About" element={<About />} />
+                <Route path="/Products" element={<Products />} />
             </Routes>
         </BrowserRouter>
     );
