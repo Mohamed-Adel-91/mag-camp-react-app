9b2e844503908b31d191b530ea3ee22b81adfc3a
Mohamed Adel <mohamed101291@gmail.com>
2023-08-29 15:54:44 +0300

    Mag App Test React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import Home from "./HomePage/home.jsx";
 import About from "./AboutPage/about.jsx";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Router,
+    Link,
+    NavLink,
+} from "react-router-dom";
 
 const App = () => {
     return (
-        <div>
-            <Home name="Mohamed Adel" img="images\gitscreen.jpg" />
-            <About />
-        </div>
+        <BrowserRouter>
+            <NavLink>
+                <Link to="/Home">Home</Link>
+                <Link to="/About">About</Link>
+            </NavLink>
+            <Routes>
+                <Route path="/Home" element={<Home />} />
+                <Route path="/About" element={<About />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
