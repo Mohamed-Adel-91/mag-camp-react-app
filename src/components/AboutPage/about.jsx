b120f30c637a50af092dd4d32a025c5d682c576e
MohamedAdel <mohamed101291@gmail.com>
2023-09-04 20:56:57 +0300

    hooks test

--- src/components/AboutPage/about.jsx
+++ src/components/AboutPage/about.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const About = () => {
+  const [count, setCount] = useState(0);
+  useEffect(()=>{
+    console.log('count: %d',count);
+    setInterval(()=>console.log('tick',1000))
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+
   return (
     <div>
       <h1>About</h1>
+      {count}<br/>
+      <button onClick={()=>setCount(count+ 1)}>+</button><br/>
+      <button onClick={()=>setCount(count- 1)}>-</button><br/>
       <h3>Test React App for Study</h3>
     </div>
   )
--- src/components/ProductsPage/products.jsx
+++ src/components/ProductsPage/products.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const products = () => {
-    function handleSubmit(){
-        alert("Handle Event is ok for button one ...!!")
+    function handleSubmit() {
+        alert("Handle Event is ok for button one ...!!");
     }
-    function handleSubmit2(e){
+    function handleSubmit2(e) {
         e.preventDefault();
-        alert("Handle Event is ok for button two ...!!")
+        alert("Handle Event is ok for button two ...!!");
     }
     const clickName2 = (name) => {
-        alert('I am '+ name)
-    }
-  return (
-    <div>
-        <h1>products</h1>
-        <button onClick={handleSubmit}>one</button>
-        <form onSubmit={handleSubmit2}>
-            <button>two</button>
-        </form>
-        <button onClick={() => clickName2("Mohamed Adel")}>three</button>
-    </div>
-  )
-}
+        alert("I am " + name);
+    };
+    return (
+        <div>
+            <h1>products</h1>
+            <button onClick={handleSubmit}>one</button>
+            <form onSubmit={handleSubmit2}>
+                <button>two</button>
+            </form>
+            <button onClick={() => clickName2("Mohamed Adel")}>three</button>
+        </div>
+    );
+};
 
-export default products
+export default products;
